ada0a656a97efa78187773c54199a905b6f555d6
thien <thiendam052@gmail.com>
2024-02-02 18:57:07 +0700

    Add navigation bottom tab

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,15 +1,11 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
+import {StyleSheet} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import HomeScreens from './HomeScreens';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Registration from '../components/management_Account/Registration';
-import Login from '../components/management_Account/Login';
-import VerifyAccount from '../components/management_Account/VerifyAccount';
-import LandingPage from '../components/management_Account/LandingPage';
-import ForgotPassword from '../components/management_Account/ForgotPassword';
-import VerifyCode from '../components/management_Account/VerifyCode';
-import LoginNew from '../components/management_Account/LoginNew';
-import HomeScreens from '../screens/HomeScreens';
-const Stack = createNativeStackNavigator();
+
 const Tab = createBottomTabNavigator();
 const Homestack = () => {
   return (
@@ -48,18 +44,18 @@ const Homestack = () => {
     </Tab.Navigator>
   );
 };
-export default function Navigation() {
+
+export default function ChatScreen() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="LandingPage" component={LandingPage} />
-        <Stack.Screen name="Registration" component={Registration} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="VerifyAccount" component={VerifyAccount} />
-        <Stack.Screen name="VerifyCode" component={VerifyCode} />
-        <Stack.Screen name="LoginNew" component={LoginNew} />
-      </Stack.Navigator>
+      <Homestack />
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
