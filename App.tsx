d4955a9f2955dbd99d5372054f5fb32f3195d8f6
quang20003 <aquangquang98765@gmail.com>
2024-02-02 16:06:10 +0700

    update UI management_Account

--- App.tsx
+++ App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Navigation from './src/navigation/Navigation';
-const queryClient = new QueryClient();
+
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Navigation />;
-    </QueryClientProvider>
-  );
+  return <Navigation />;
 }
